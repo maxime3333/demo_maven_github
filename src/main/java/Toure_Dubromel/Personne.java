85b9446fd1564723c22e704bbbc66315dad2e121
maxime3333 <dmaxm3@gmail.com>
2026-04-17 13:56:03 +0200

    classPersonne

--- src/main/java/Toure_Dubromel/Personne.java
+++ src/main/java/Toure_Dubromel/Personne.java
@@ -6,4 +6,18 @@ public abstract class Personne {
     public String adresse;
     public String contact;
 
+    public Personne(String identifiant, String nom, String adresse, String contact) {
+        this.identifiant = identifiant;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.contact = contact;
+    }
+
+    public abstract void obtenirInfos(); // Méthode demandée dans le sujet
+
+    // Getters
+    public String getIdentifiant() { return identifiant; }
+    public String getNom() { return nom; }
 }
+
+
